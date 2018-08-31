4899013eb1e1fcc6b8e7bccffb7684835186fba1
Stefan Balke <stefan@balke.at>
2018-08-31 14:03:53 +0200

    cleanup and docs and more cleanup

--- leap_control/gui.py
+++ leap_control/gui.py
@@ -1,8 +1,6 @@
 import os
 from os import listdir
 from os.path import isfile, join
-from subprocess import Popen, PIPE
-import re
 
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -11,11 +9,6 @@ from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse
 from kivy.clock import Clock
 from kivy.uix.dropdown import DropDown
-
-# import leap motion (unfortunately there is no pip package for installing)
-# src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-# arch_dir = './lib/x64' if sys.maxsize > 2**32 else './lib/x86'
-# sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 import Leap
 import numpy as np
@@ -58,75 +51,99 @@ class MyPaintWidget(Widget):
         super(MyPaintWidget, self).__init__()
 
         self.controller = Leap.Controller()
-        self.positions = []
-        self.max_len_pos = 20
+        # controller's coordinate system
+        self.x_lim = [-200, 200]
+        self.y_lim = [100, 400]
 
+        # history of positions
+        self.positions = [(0.5, 0.5), ]
+        self.max_len_pos = 40
+        self.smoothing_factor = 0.3
+
+        # configure tempo factor and velocity range
+        self.tf_min = 0.5
+        self.tf_max = 2.0
+        self.vel_min = 0
+        self.vel_max = 128
 
         self.th = th
         self.size = Window.size
 
-
     def update(self, *args):
-        color = (1, 0, 0)
-
+        # get current frame
         frame = self.controller.frame()
-
-        y_lim = [100, 400]
-        x_lim = [-200, 200]
-
-        velocity = np.linspace(100, 400, num=128)
-        tempo = np.linspace(0.5, 2, num=400)
-
-        x_loc = np.linspace(-200, 200, num=400)
 
         # only access hand if one is available
         if len(frame.hands) > 0:
-
-            if len(self.positions) >= self.max_len_pos:
-                self.positions = self.positions[0:-1]
+            # limit list of positions to max_len_pos
+            self.positions = self.positions[:self.max_len_pos]
 
             # get the first hand
             hand = frame.hands[0]
 
-            # extract palm position (0 ... x-axis, 1 ... y-axis, 2 ... z-axis)
-            pos = [hand.palm_position[0], hand.palm_position[1]]
+            # get postion and smooth
+            pos = self.get_pos(hand)
+            pos = self.smooth(pos)
 
-            # set position limits
-            if pos[0] < x_lim[0]:
-                pos[0] = x_lim[0]
-            elif pos[0] > x_lim[1]:
-                pos[0] = x_lim[1]
+            # map position to tempo-factor and velocity
+            cur_tempo_factor = -(self.tf_max - self.tf_min) * pos[0] + 2.0
+            cur_velocity = (self.vel_max - self.vel_min) * pos[1] + self.vel_min
 
-            if pos[1] < y_lim[0]:
-                pos[1] = y_lim[0]
-            elif pos[1] > y_lim[1]:
-                pos[1] = y_lim[1]
+            # set the MIDI playback accordingly
+            self.th.set_tempo(cur_tempo_factor)
+            self.th.set_velocity(int(cur_velocity))
 
-            vel = sum(velocity < pos[1])
-
-            temp = tempo[sum(x_loc > pos[0])]
-
-            self.th.set_velocity(vel)
-            self.th.set_tempo(temp)
-
-            pos[1] = pos[1]/300.0 - 1./3
-            pos[0] = pos[0]/400.0 + 0.5
-
-            # store position at the beginning of the list
+            # store current position at the beginning of the list
             self.positions.insert(0, pos)
 
-            # print(note, vel)
-            # msg = mido.Message('note_on', note=note, velocity=vel)
+            # draw worm as red trajectory
+            self.draw_worm((1, 0, 0))
 
-            print(pos[0], pos[1])
+    def draw_worm(self, color):
+        """Draw worm trajectory."""
 
-            self.canvas.clear()
-            with self.canvas:
-                for i, p in enumerate(self.positions):
-                    Color(*color, mode='rgb', a=1-i*(1.0/self.max_len_pos))
-                    d = 30.
-                    size = d-(d/self.max_len_pos)*i
-                    Ellipse(pos=(p[0]*self.size[0], p[1]*self.size[1]), size=(size, size))
+        self.canvas.clear()
+
+        with self.canvas:
+            for i, p in enumerate(self.positions):
+                # color with adjusted alpha channel
+                # the older, make more transparent
+                Color(*color, mode='rgb', a=1 - i * (1.0 / self.max_len_pos))
+
+                # circle size depends on position in list
+                # the older, the smaller
+                d = 30.0
+                size = d - (d / self.max_len_pos) * i
+
+                # draw circle
+                Ellipse(pos=(p[0] * self.size[0], p[1] * self.size[1]), size=(size, size))
+
+    def get_pos(self, hand):
+        """Get hand position from LeapMotion"""
+
+        # extract palm position (0 ... x-axis, 1 ... y-axis, 2 ... z-axis)
+        pos = [hand.palm_position[0], hand.palm_position[1]]
+
+        # set position limits
+        pos[0] = max(pos[0], self.x_lim[0])
+        pos[0] = min(pos[0], self.x_lim[1])
+
+        pos[1] = max(pos[1], self.y_lim[0])
+        pos[1] = min(pos[1], self.y_lim[1])
+
+        # project sensor data to [0, 1]
+        pos[0] = pos[0] / 400.0 + 0.5  # x
+        pos[1] = pos[1] / 300.0 - 1./3  # y
+
+        return pos
+
+    def smooth(self, pos):
+        """Smooth hand trajectory with exponential moving average filter."""
+
+        pos[0] = self.smoothing_factor * pos[0] + (1 - self.smoothing_factor) * self.positions[0][0]
+        pos[1] = self.smoothing_factor * pos[1] + (1 - self.smoothing_factor) * self.positions[0][1]
+
+        return pos
 
 
 class MyPaintApp(App):
@@ -169,4 +186,5 @@ class MyPaintApp(App):
 
 
 if __name__ == '__main__':
+    # Window.fullscreen = True
     MyPaintApp().run()
--- leap_control/midi_thread.py
+++ leap_control/midi_thread.py
@@ -1,16 +1,18 @@
+"""
+    Threading class for MIDI playback.
+"""
 import threading
 import time
 import mido
 
-class MidiThread(threading.Thread):
 
+class MidiThread(threading.Thread):
     def __init__(self, midi_path, midi_port):
         threading.Thread.__init__(self)
         self.midi = midi_path
         self.midi_port = midi_port
         self.vel = None
         self.tempo = 1
-
 
     def set_velocity(self, vel):
         self.vel = vel
@@ -19,18 +21,13 @@ class MidiThread(threading.Thread):
         self.tempo = tempo
 
     def run(self):
-
         with mido.open_output(self.midi_port) as outport:
-
             for msg in mido.MidiFile(self.midi):
-
                 play_msg = msg
                 if msg.type == 'note_on':
-
                     if msg.velocity != 0 and self.vel is not None:
                         play_msg = msg.copy(velocity=self.vel)
 
                 time.sleep(play_msg.time*self.tempo)
 
                 outport.send(play_msg)
-
