c375071971105e2fe4dc272637bff5630918c9b7
Florian Henkel <florian.henkel@jku.at>
2018-10-31 11:24:03 +0100

    - powermate access simplified

--- powermate/knob_thread.py
+++ powermate/knob_thread.py
@@ -1,26 +1,38 @@
-import glob
-from powermate.powermate import PowerMateBase
 from threading import Thread
-
-
-class ExamplePowerMate(PowerMateBase):
-    def __init__(self, path, progress_bar):
-        super(ExamplePowerMate, self).__init__(path)
-        self.pos = 0
-        self.progress_bar = progress_bar
-
-    def rotate(self, rotation):
-        # one rotation is around 90 rotation ticks, the following lines scale the position to range [0,10]
-        self.pos = max(0, min(self.pos+rotation, 90))
-        # self.progress_bar.set_value(int(self.pos/9))
-        self.progress_bar.set_value(self.pos)
-
+import usb
 
 
 class KnobThread(Thread):
     def __init__(self, progress_bar):
         Thread.__init__(self)
-        self.pm = ExamplePowerMate(glob.glob('/dev/input/by-id/*PowerMate*')[0], progress_bar)
+
+        self.pos = 0
+        self.progress_bar = progress_bar
+
+        VENDOR = 0x077d  # 1917
+        PRODUCT = 0x0410  # 1040
+
+        self.dev = usb.core.find(idVendor=VENDOR, idProduct=PRODUCT)
+
+        if self.dev is None:
+            raise Exception("Could not find Griffin PowerMate.")
+
+        # otherwise the kernel is busy
+        if self.dev.is_kernel_driver_active(0):
+            self.dev.detach_kernel_driver(0)
+
+        self.dev.set_configuration()
 
     def run(self):
-        self.pm.run()
+        # self.pm.run()
+        while self.dev.read(129, 6, timeout=99999999999)[0] == 0:
+
+            rot = self.dev.read(129, 6, timeout=99999999999)[1]
+
+            # other direction is encoded as 255
+            if rot >= 127:
+                rot = -1  # for opposite direction
+
+            self.pos = max(0, min(self.pos + rot, 90))
+            self.progress_bar.set_value(self.pos)
+
